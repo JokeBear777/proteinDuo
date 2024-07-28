a35c48b204c02a600d20326f131205894bf1840a
unikal1 <49333855+unikal1@users.noreply.github.com>
2024-07-28 15:40:01 +0900

    Initial commit

---
+++ src/main/java/com/proteinduo/ProteinDuoApplication.java
@@ -0,0 +1,13 @@
+package com.proteinduo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProteinDuoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProteinDuoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proteinduo/ProteinDuoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proteinduo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProteinDuoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
