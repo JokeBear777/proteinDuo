116f48df9a14588536075d52c43c07f95e48c1d6
JokeBear777 <korea9955@naver.com>
2024-08-09 12:59:11 +0900

    [TEST] add securityConfig TEst

---
+++ src/main/java/com/proteinduo/domain/memberManage/dto/MemberInfoRequest.java
@@ -0,0 +1,45 @@
+package com.proteinduo.domain.memberManage.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * packageName    : com.proteinduo.domain.memberManage.dto
+ * fileName       : memberInfoRequest
+ * author         : 82102
+ * date           : 2024-08-06
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-08-06        82102       최초 생성
+ */
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class MemberInfoRequest {
+
+    private String username;
+
+    private String email;
+
+    private String gender;
+
+    private Integer height;
+
+    private Integer weight;
+
+    private Integer muscleMass;
+
+    private Integer bodyFat;
+
+    private Integer bmi;
+
+    private Integer bodyFatPercentage;
+
+
+}
